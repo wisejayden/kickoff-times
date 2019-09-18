46b44be91e5e31ae0d99c573606c1a7d49b4f17e
Jayden Wise <jayden.wise@gravityandstorm.de>
2019-09-18 23:27:19 +0200

    Removed excess items, added favicon

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -6,6 +6,8 @@ const Footer = () => {
       <small>&copy; Copyright 2019, Jayden Wise</small>
 
       <div className="attribution">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div>
+      <div className="attribution">Favicon found on <a href="https://www.flaticon.com/authors/freepik" title="icons8">icons8</a> at <a href="https://icons8.com/"     title="Flaticon">icons8.com</a></div>
+
     </footer>
   )
 }
