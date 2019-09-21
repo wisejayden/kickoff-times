2dd237b48b0552c097804b19174bbac598144d6a
Jayden Wise <jayden.wise@gravityandstorm.de>
2019-09-21 16:44:27 +0200

    Fixed links

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
     <footer className="Footer">
       <small>&copy; Copyright 2019, Jayden Wise</small>
 
-      <div className="attribution">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div>
-      <div className="attribution">Favicon found on <a href="https://www.flaticon.com/authors/freepik" title="icons8">icons8</a> at <a href="https://icons8.com/"     title="Flaticon">icons8.com</a></div>
+      <div className="attribution">Icons made by <a href="https://www.flaticon.com/authors/freepik" target="_blank" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div>
+      <div className="attribution">Favicon found on <a href="https://icons8.com/" target="_blank" title="icons8">icons8</a> at <a href="https://icons8.com/"     title="Flaticon">icons8.com</a></div>
 
     </footer>
   )
