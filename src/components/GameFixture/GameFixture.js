9447aeb621e3e732e917e885ca1a74fae8f75a8d
Jayden Wise <jayden.wise@gravityandstorm.de>
2019-10-02 15:25:49 +0200

    Toggle Lineup notice on click

--- src/components/GameFixture/GameFixture.js
+++ src/components/GameFixture/GameFixture.js
@@ -1,222 +1,261 @@
-import React, { useContext, useState, useEffect } from 'react';
-import './GameFixture.scss';
-import Ratings from '../Ratings/Ratings';
-import cityTimezones from 'city-timezones';
-import moment from 'moment';
-import tz from 'moment-timezone';
-
-
+import React, { useContext, useState, useEffect } from "react";
+import "./GameFixture.scss";
+import Ratings from "../Ratings/Ratings";
+import cityTimezones from "city-timezones";
+import moment from "moment";
+import tz from "moment-timezone";
 
 import { observer } from "mobx-react-lite";
 import { StoreContext } from "../../index";
-import {toJS} from 'mobx';
+import { toJS } from "mobx";
 
+const GameFixture = observer(
+  ({ gameData, pool, image, matchesView, ...props }) => {
+    const [lineupState, changeLineupState] = useState(false);
+    const [mouseOverText, addMouseOverText] = useState("");
+    const [lineupVersion, setLineupVersion] = useState("");
+    const [gameId, setGameId] = useState("");
+    const [fixtureHasCurrentLineup, setFixtureHasCurrentLineup] = useState(
+      false
+    );
 
+    const store = useContext(StoreContext).AppStore;
 
+    // iso-8601 date format
+    const scheduled = moment(gameData.scheduled);
 
-const GameFixture = observer(({gameData, pool, image, matchesView, ...props}) => {
-  const [lineupState, changeLineupState] = useState(false);
-  const [mouseOverText, addMouseOverText] = useState('');
-  const [lineupVersion, setLineupVersion] = useState('');
-  const [gameId, setGameId] =  useState('');
-  const [fixtureHasCurrentLineup, setFixtureHasCurrentLineup] = useState(false);
+    //const userTimeIn12HourTime = scheduled.format('h:mm a')
+    const userTime = scheduled.format("HH:mm");
 
+    //Need to find a better solution in case the city is not found. Large country may screw with the timezone.
+    const cityLookup =
+      cityTimezones.lookupViaCity(gameData.venue.city_name).length > 0
+        ? cityTimezones.lookupViaCity(gameData.venue.city_name)[0]
+        : cityTimezones.findFromCityStateProvince(
+            gameData.venue.country_name
+          )[0];
+    const localTime = scheduled.tz(cityLookup.timezone).format("HH:mm");
 
+    const shortenedWeekday = store.weekArray[scheduled.day()].substring(0, 3);
+    const imageUrlArray = [
+      `/images/country/${image[0]}`,
+      `/images/country/${image[1]}`
+    ];
 
+    const matchPassed = moment() > scheduled ? true : false;
 
-  const store = useContext(StoreContext).AppStore;
-
-
-  // iso-8601 date format
-  const scheduled = moment(gameData.scheduled);
-
-  //const userTimeIn12HourTime = scheduled.format('h:mm a')
-  const userTime = scheduled.format('HH:mm');
-
-  //Need to find a better solution in case the city is not found. Large country may screw with the timezone.
-  const cityLookup = cityTimezones.lookupViaCity(gameData.venue.city_name).length > 0 ? cityTimezones.lookupViaCity(gameData.venue.city_name)[0] : cityTimezones.findFromCityStateProvince(gameData.venue.country_name)[0];
-  const localTime = scheduled.tz(cityLookup.timezone).format('HH:mm');
-
-  const shortenedWeekday = store.weekArray[scheduled.day()].substring(0,3);
-  const imageUrlArray = [`/images/country/${image[0]}`, `/images/country/${image[1]}`]
-
-  const matchPassed = moment() > scheduled ? true : false;
-
-  const determinePoolColour = () => {
-    if(pool === "Pool A") {
-      return {color: '#54c8e8', fontWeight: "bold"};
-    }
-    if(pool === "Pool B") {
-      return {color: '#f4436c', fontWeight: "bold"};
-    }
-    if(pool === "Pool C") {
-      return {color: '#2ed9c3', fontWeight: "bold"};
-    }
-    if(pool === "Pool D") {
-      return {color: '#993dbb', fontWeight: "bold"};
-    }
-  }
-
-
-  const determineStadiumName = () => {
-    for(let i = 0; i < store.checkStadiumName.length; i++) {
-      if (gameData.venue.city_name === store.checkStadiumName[i][0]) {
-        return store.checkStadiumName[i][1];
+    const determinePoolColour = () => {
+      if (pool === "Pool A") {
+        return { color: "#54c8e8", fontWeight: "bold" };
       }
-    }
-  }
-
-  const toggleLineup = () => {
-    if(matchesView && gameFixtureClickable) {
-      changeLineupState(!lineupState);
-    }
-  }
-  let gameFixtureClickable;
-  let gameFixtureHeight;
-  // let fixtureHasCurrentLineup = store.lineup.sport_event.id === gameData.id;
-  // let fixtureHasCurrentLineup;
-  // if(store.lineup.sport_event.id === gameData.id){
-  //   setLineupVersion("primary");
-  //   let fixtureHasCurrentLineup = true;
-  // } else {
-  //   for(let key in store.backup)
-  // }
-  useEffect(() => {
-    let id = store.checkForLineup(gameData.id);
-    if(id) {
-      // fixtureHasCurrentLineup = true;
-      setLineupVersion("backup");
-      setGameId(id);
-      setFixtureHasCurrentLineup(true);
-    }
-  }, [gameId]);
-
-  let homeTeam = gameData.competitors[0].name;
-  let awayTeam;
-
-
-  if (fixtureHasCurrentLineup && lineupState) {
-    gameFixtureHeight = {height: '51rem'};
-  } else if(fixtureHasCurrentLineup) {
-    gameFixtureHeight = {height: '11rem'};
-  } else {
-    gameFixtureHeight = {height: '10rem'};
-  }
-  (fixtureHasCurrentLineup && matchesView) || !matchesView ? gameFixtureClickable = {cursor: 'pointer'} : gameFixtureClickable = {};
-
-
-  return (
-    <div className="GameFixture" onMouseOver={() => addMouseOverText("Rate")} onMouseOut={() => addMouseOverText('')} style={Object.assign(gameFixtureClickable, gameFixtureHeight)}>
-      {matchesView &&
-        <div onClick={() => toggleLineup()} className="game-fixture-time-container">
-
-        <span>{shortenedWeekday} {scheduled.date()} {store.monthArray[scheduled.month()]}</span>
-        <span className="your-time">{userTime} Your Time</span>
-
-       </div>
-
+      if (pool === "Pool B") {
+        return { color: "#f4436c", fontWeight: "bold" };
       }
+      if (pool === "Pool C") {
+        return { color: "#2ed9c3", fontWeight: "bold" };
+      }
+      if (pool === "Pool D") {
+        return { color: "#993dbb", fontWeight: "bold" };
+      }
+    };
 
+    const determineStadiumName = () => {
+      for (let i = 0; i < store.checkStadiumName.length; i++) {
+        if (gameData.venue.city_name === store.checkStadiumName[i][0]) {
+          return store.checkStadiumName[i][1];
+        }
+      }
+    };
 
-       <div className="game-fixture-playing" onClick={() => toggleLineup()} style={matchesView === false ? {height: '100%', marginTop: '1.3rem'} : {}}>
-            <img className="country-circle" src={imageUrlArray[0]} alt={gameData.competitors[0].name + " country flag"}/>
-            <div className="match-information">
-              {!matchesView && matchPassed &&
-                <Ratings id={gameData.id} gameData={gameData} mouseOverText={mouseOverText}/>
-              }
-              {
-                !matchesView && !matchPassed &&
-                <div className="space-saver"></div>
-              }
-              <p className="competitors">{gameData.competitors[0].name} v {gameData.competitors[1].name}</p>
-              <span style={determinePoolColour()}>{pool} </span>
-              <span>{determineStadiumName()}</span>
-              {matchesView &&
-                <p>{localTime} Local Time</p>
-              }
-              {fixtureHasCurrentLineup && matchesView &&
-                <p className="lineup-available">Lineups available! Click to show..</p>
-              }
+    const toggleLineup = () => {
+      if (matchesView && gameFixtureClickable) {
+        changeLineupState(!lineupState);
+      }
+    };
+    let gameFixtureClickable;
+    let gameFixtureHeight;
+    // let fixtureHasCurrentLineup = store.lineup.sport_event.id === gameData.id;
+    // let fixtureHasCurrentLineup;
+    // if(store.lineup.sport_event.id === gameData.id){
+    //   setLineupVersion("primary");
+    //   let fixtureHasCurrentLineup = true;
+    // } else {
+    //   for(let key in store.backup)
+    // }
+    useEffect(() => {
+      let id = store.checkForLineup(gameData.id);
+      if (id) {
+        // fixtureHasCurrentLineup = true;
+        setLineupVersion("backup");
+        setGameId(id);
+        setFixtureHasCurrentLineup(true);
+      }
+    }, [gameId]);
 
-            </div>
+    let homeTeam = gameData.competitors[0].name;
+    let awayTeam;
 
-            <img className="country-circle" src={imageUrlArray[1]} alt={gameData.competitors[1].name + " country flag"}/>
-       </div>
-       {fixtureHasCurrentLineup && lineupState && matchesView && lineupVersion === "primary" &&
-              <div className="lineups-container" >
-                <ul>
-                  {store.lineup.lineups[0].starting_lineup.map(pos=> {
-                    return (
-                      <li>{pos.player.jersey_number}. {pos.player.name.split(',').reverse().join(" ")}</li>
-                    )
-                  })}
-                  <br></br>
-                   {store.lineup.lineups[0].substitutes.map(pos=> {
-                    return (
-                      <li>{pos.player.jersey_number}. {pos.player.name.split(',').reverse().join(" ")}</li>
-                    )
-                  })}
-                </ul>
-                <ul>
-                  {store.lineup.lineups[1].starting_lineup.map(pos=> {
-                    return (
-                      <li>{pos.player.jersey_number}. {pos.player.name.split(',').reverse().join(" ")}</li>
-                    )
-                  })}
-                  <br></br>
-                   {store.lineup.lineups[1].substitutes.map(pos=> {
-                    return (
-                      <li>{pos.player.jersey_number}. {pos.player.name.split(',').reverse().join(" ")}</li>
-                    )
-                  })}
-                </ul>
-              </div>
-            }
+    if (fixtureHasCurrentLineup && lineupState) {
+      gameFixtureHeight = { height: "51rem" };
+    } else if (fixtureHasCurrentLineup) {
+      gameFixtureHeight = { height: "11rem" };
+    } else {
+      gameFixtureHeight = { height: "10rem" };
+    }
+    (fixtureHasCurrentLineup && matchesView) || !matchesView
+      ? (gameFixtureClickable = { cursor: "pointer" })
+      : (gameFixtureClickable = {});
 
-            {fixtureHasCurrentLineup && lineupState && matchesView && lineupVersion === "backup" &&
-            <div className="lineups-container" >
-              <ul>
-                {
-                  store.backup[gameId][0].starters.map(pos => {
-                    return (
-                      <li>{pos}</li>
-                    )
-                  })
-                }
-                <br></br>
-                {
-                  store.backup[gameId][0].finishers.map(pos => {
-                    return (
-                      <li>{pos}</li>
-                    )
-                  })
-                }
-              </ul>
-              <ul>
-                {
-                  store.backup[gameId][1].starters.map(pos => {
-                    return (
-                      <li>{pos}</li>
-                    )
-                  })
-                }
-                <br></br>
-                {
-                  store.backup[gameId][1].finishers.map(pos => {
-                    return (
-                      <li>{pos}</li>
-                    )
-                  })
-                }
-              </ul>
-            </div>
+    return (
+      <div
+        className="GameFixture"
+        onMouseOver={() => addMouseOverText("Rate")}
+        onMouseOut={() => addMouseOverText("")}
+        style={Object.assign(gameFixtureClickable, gameFixtureHeight)}
+      >
+        {matchesView && (
+          <div
+            onClick={() => toggleLineup()}
+            className="game-fixture-time-container"
+          >
+            <span>
+              {shortenedWeekday} {scheduled.date()}{" "}
+              {store.monthArray[scheduled.month()]}
+            </span>
+            <span className="your-time">{userTime} Your Time</span>
+          </div>
+        )}
 
+        <div
+          className="game-fixture-playing"
+          onClick={() => toggleLineup()}
+          style={
+            matchesView === false ? { height: "100%", marginTop: "1.3rem" } : {}
           }
+        >
+          <img
+            className="country-circle"
+            src={imageUrlArray[0]}
+            alt={gameData.competitors[0].name + " country flag"}
+          />
+          <div className="match-information">
+            {!matchesView && matchPassed && (
+              <Ratings
+                id={gameData.id}
+                gameData={gameData}
+                mouseOverText={mouseOverText}
+              />
+            )}
+            {!matchesView && !matchPassed && (
+              <div className="space-saver"></div>
+            )}
+            <p className="competitors">
+              {gameData.competitors[0].name} v {gameData.competitors[1].name}
+            </p>
+            <span style={determinePoolColour()}>{pool} </span>
+            <span>{determineStadiumName()}</span>
+            {matchesView && <p>{localTime} Local Time</p>}
+            {fixtureHasCurrentLineup &&
+              matchesView &&
+              lineupState === false && (
+                <p className="lineup-available">
+                  Lineups available! Click to show..
+                </p>
+              )}
+          </div>
 
+          <img
+            className="country-circle"
+            src={imageUrlArray[1]}
+            alt={gameData.competitors[1].name + " country flag"}
+          />
+        </div>
+        {fixtureHasCurrentLineup &&
+          lineupState &&
+          matchesView &&
+          lineupVersion === "primary" && (
+            <div className="lineups-container">
+              <ul>
+                {store.lineup.lineups[0].starting_lineup.map(pos => {
+                  return (
+                    <li>
+                      {pos.player.jersey_number}.{" "}
+                      {pos.player.name
+                        .split(",")
+                        .reverse()
+                        .join(" ")}
+                    </li>
+                  );
+                })}
+                <br></br>
+                {store.lineup.lineups[0].substitutes.map(pos => {
+                  return (
+                    <li>
+                      {pos.player.jersey_number}.{" "}
+                      {pos.player.name
+                        .split(",")
+                        .reverse()
+                        .join(" ")}
+                    </li>
+                  );
+                })}
+              </ul>
+              <ul>
+                {store.lineup.lineups[1].starting_lineup.map(pos => {
+                  return (
+                    <li>
+                      {pos.player.jersey_number}.{" "}
+                      {pos.player.name
+                        .split(",")
+                        .reverse()
+                        .join(" ")}
+                    </li>
+                  );
+                })}
+                <br></br>
+                {store.lineup.lineups[1].substitutes.map(pos => {
+                  return (
+                    <li>
+                      {pos.player.jersey_number}.{" "}
+                      {pos.player.name
+                        .split(",")
+                        .reverse()
+                        .join(" ")}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          )}
 
-    </div>
-  )
-});
-
+        {fixtureHasCurrentLineup &&
+          lineupState &&
+          matchesView &&
+          lineupVersion === "backup" && (
+            <div className="lineups-container">
+              <ul>
+                {store.backup[gameId][0].starters.map(pos => {
+                  return <li>{pos}</li>;
+                })}
+                <br></br>
+                {store.backup[gameId][0].finishers.map(pos => {
+                  return <li>{pos}</li>;
+                })}
+              </ul>
+              <ul>
+                {store.backup[gameId][1].starters.map(pos => {
+                  return <li>{pos}</li>;
+                })}
+                <br></br>
+                {store.backup[gameId][1].finishers.map(pos => {
+                  return <li>{pos}</li>;
+                })}
+              </ul>
+            </div>
+          )}
+      </div>
+    );
+  }
+);
 
 export default GameFixture;
