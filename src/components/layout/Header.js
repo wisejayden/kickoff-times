35826753e7ad35eb1e94b15d26b7f8be3e60e858
Jayden Wise <jayden.wise@gravityandstorm.de>
2019-10-12 17:31:56 +0200

    Header styling changes

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,34 +1,34 @@
-import React, {useContext} from 'react';
-import './Header.scss';
-import {Link} from 'react-router-dom';
+import React, { useContext } from "react";
+import "./Header.scss";
+import { Link } from "react-router-dom";
 import { IconContext } from "react-icons";
-import { MdLocationOn } from 'react-icons/md';
-import { FaUser } from 'react-icons/fa';
+import { MdLocationOn } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
 import { observer } from "mobx-react-lite";
 import { StoreContext } from "../../index";
+import Headroom from "react-headroom";
 
-const tempLogo = './images/templogo.png';
-
+const tempLogo = "./images/templogo.png";
 
 const Header = observer(() => {
   const store = useContext(StoreContext).AppStore;
 
   const currentLocale = "Berlin";
   return (
-    <header className="Header">
-        
-        {/* <div className="locale-container"> */}
-          {/* <IconContext.Provider value={{className: "locale-icon" }}><MdLocationOn /></IconContext.Provider> */}
-          {/* <span>{currentLocale}</span> */}
-        {/* </div> */}
-       
-        {/* <Link to="/" ><img id="logo" src={tempLogo} alt="Kickoff Times Logo"/></Link> */}
-        <Link to="/" onClick={store.clearFilterData}><h1>KickOff Times</h1></Link>
+    <div className="Header">
+      {/* <div className="locale-container"> */}
+      {/* <IconContext.Provider value={{className: "locale-icon" }}><MdLocationOn /></IconContext.Provider> */}
+      {/* <span>{currentLocale}</span> */}
+      {/* </div> */}
+
+      {/* <Link to="/" ><img id="logo" src={tempLogo} alt="Kickoff Times Logo"/></Link> */}
+      <Link to="/" onClick={store.clearFilterData}>
+        <h1>KickOff Times</h1>
+      </Link>
 
       {/* <IconContext.Provider value={{ className: "profile-icon" }}><FaUser /></IconContext.Provider> */}
-
-    </header>
-  )
+    </div>
+  );
 });
 
 export default Header;
