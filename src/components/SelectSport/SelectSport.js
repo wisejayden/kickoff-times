cab411da31e24c7413c95401acd43e54b194d568
Jayden Wise <jayden.wise@gravityandstorm.de>
2019-11-01 17:44:13 +0100

    Added a SelectSport component to display on home

---
+++ src/components/SelectSport/SelectSport.js
@@ -0,0 +1,33 @@
+import React, { useContext, useEffect } from 'react';
+import { StoreContext } from "../../index";
+import { observer } from "mobx-react-lite";
+import SportTile from '../SportTile/SportTile';
+import './SelectSport.scss';
+import { withRouter } from "react-router-dom";
+
+
+import { toJS } from "mobx";
+
+
+
+const SelectSport = withRouter(observer(({view, ...props}) => {
+  const apiStore = useContext(StoreContext).ApiStore;
+
+
+  let content;
+  console.log("!", toJS(apiStore.allSportsArray));
+  content = apiStore.allSportsArray.map((sport, i) => {
+     const {strSportThumb, strSport, idSport, strUrl} = sport;
+        return(
+          <SportTile image={strSportThumb} sportName={strSport} id={idSport} url={strUrl} key={i}/>
+        )
+      })
+
+  return(
+    <div className="SelectSport">
+      {content}
+    </div>
+  )
+}));
+
+export default SelectSport;
