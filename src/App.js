16f4b98cf669df9968dc187722d8561dca2da5fb
Jayden Wise <jayden.wise@gravityandstorm.de>
2019-11-01 17:45:41 +0100

    Added Api Calls on Application mount

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
-import React from 'react';
+import React, {useEffect, useContext} from 'react';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import SelectSport from './components/SelectSport/SelectSport';
 import FrontPage from './components/frontpage/Main';
 import SportContent from './components/sport/SportContent';
 import NotFound from './components/NotFound/NotFound';
+import SelectTournament from './components/SelectTournament/SelectTournament';
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { observer } from "mobx-react-lite";
+import { StoreContext } from "./index";
+
+
 import './App.scss';
 
+const sports = ["Rugby"];
 
-function App(props) {
+
+const App = observer((props) => {
+  const apiStore = useContext(StoreContext).ApiStore;
+
+  useEffect(() => {
+    apiStore.getSports();
+  }, [])
+
+  // <Route path="/matches" component={FrontPage} />
+  // <Route path="/pools" component={FrontPage} />
+  // <Route path="/ratings" component={FrontPage} />
+
   return (
     <Router>
       <div className="App">
         <Header />
         <div className="body">
         <Switch>
-          <Redirect exact from="/" to="/matches" />
-          <Route path="/matches" component={FrontPage} />
-          <Route path="/pools" component={FrontPage} />
-          <Route path="/ratings" component={FrontPage} />
-          <Route component={NotFound} />
+          <Route path="/:sport" render={(props) => <SelectTournament {...props} view={"Tournament"}/>} />
+
+
+        <Route path="/" render={(props) => <SelectSport {...props} view={"Sport"}/>} />
 
 
 
-          <Route
-           path="/:sport"
-           component={SportContent}
-         />
+         <Route component={NotFound} />
+
+
 
         </Switch>
         <Footer />
