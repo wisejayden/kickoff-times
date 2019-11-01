832ba6b2d470229611b1fb6e71f9a8be1c08eee8
Jayden Wise <jayden.wise@gravityandstorm.de>
2019-11-01 17:45:27 +0100

    Added ApiStore to the RootStore

--- src/stores/RootStore.js
+++ src/stores/RootStore.js
@@ -1,9 +1,11 @@
 import LoginStore from './LoginStore';
 import AppStore from './AppStore';
+import ApiStore from './ApiStore'; 
 
 export default class RootStore {
   constructor() {
     this.LoginStore = new LoginStore(this);
     this.AppStore = new AppStore(this);
+    this.ApiStore = new ApiStore(this);
   }
 }
