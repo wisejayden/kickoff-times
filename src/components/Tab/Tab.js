27bcf204114a3b8711d858d4234b2af73f2144d9
Jayden Wise <jayden.wise@gravityandstorm.de>
2020-03-20 18:23:24 +0200

    Code cleanup Tab component

--- src/components/Tab/Tab.js
+++ src/components/Tab/Tab.js
@@ -3,15 +3,8 @@ import { Link } from 'react-router-dom';
 import './Tab.scss';
 import { withRouter} from "react-router-dom";
 
-
-
-
-
-
-
 const Tab = withRouter(({ ...props}) => {
   const path = props.history.location.pathname;
-  
   return (
     <div className="Tab">
         <Link className={path === "/matches" ? "Tab-link active" : "Tab-link"} to={"/matches"}><span>Matches</span></Link>
