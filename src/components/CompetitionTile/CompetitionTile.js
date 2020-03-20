08ce650311c6fd11e95ab87a6286f6ab2511e033
Jayden Wise <jayden.wise@gravityandstorm.de>
2020-03-20 18:32:02 +0200

    CompetitionTile cleanup

--- src/components/CompetitionTile/CompetitionTile.js
+++ src/components/CompetitionTile/CompetitionTile.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import rwc from '../../rwc-schedule.json';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-
+import { BrowserRouter as Link } from "react-router-dom"
 import './CompetitionTile.scss';
-
 const rwcLogo = '/images/rwc-logo.png';
 
-
-const CompetitionTile = (sport, comp, ...props) => {
-  
+const CompetitionTile = () => {
     return (
         <div className="CompetitionTile">
             <Link to="/rugby/rugbyworldcup/"><img src={rwcLogo} alt="Rugby World Cup 2019"/></Link>                        
